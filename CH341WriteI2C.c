4d779f3f09f373152d9802c84f41e691a56965fe
sepiqon <kalinuxhacker@gmail.com>
2023-06-10 21:41:58 +0200

    testingCH341Read from NodeMcu

--- CH341WriteI2C.c
+++ CH341WriteI2C.c
@@ -31,15 +31,14 @@ int main(int argc, char* argv[])
     else {
         printf("[INFO] Magistrala I2C dziala.\n");
     }
-    CH341SetStream(0, 0);
-    UINT8 tosend[] = { (0x08 << 1), 84, 97, 106, 110, 121, 32, 116, 101, 107, 115, 116, 32, 110, 119, 109, 32, 99, 122, 121, 32, 115, 105, 101, 32, 117, 100, 97, 32, 112, 111, 119, 111, 100, 122, 101, 110, 105, 97, 32, 58, 41, 33 };
-    UINT8 toread[] = { 0x00 };
-    boolean test1 = CH341StreamI2C(0, 32, &tosend[0], 0, NULL);
-    if (test1) {
-        printf("UDALO SIE\n");
-    }
+    UINT8 tosend[] = { (0x08 << 1), 42, 97, 106, 110, 121, 32, 116, 101, 107, 115, 116, 32, 110, 119, 109, 32, 99, 122, 121, 32, 115, 105, 101, 32, 117, 100, 97, 32, 112, 111, 119, 111, 100, 122, 101, 110, 105, 97, 32, 58, 41, 33 };
+    UINT8 toread[6] = { };
+    toread[5] = 0;
+    boolean test1 = CH341StreamI2C(0, 1, &tosend[0], 5, &toread[0]);
 
-    printf("Hej %d NACISNIJ KLAWISZ BY ZAKONCZYC...", open);
+    printf("Uzyskano: %s\n", toread);
+    printf("Hej NACISNIJ KLAWISZ BY ZAKONCZYC...");
+    CH341CloseDevice(0);
     getchar();
     return 0;
 }
